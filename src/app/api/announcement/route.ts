905019e66fcd3007635c2a4c1ba00e077e983445
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2025-04-13 18:02:44 +0200

    Adds .env, installs edge configs

--- src/app/api/announcement/route.ts
+++ src/app/api/announcement/route.ts
@@ -1,9 +1,19 @@
 import {NextResponse} from 'next/server';
+import {createClient} from '@vercel/edge-config';
+
+export const runtime = 'edge';
 
 export async function GET() {
   try {
-
-    const announcement = process.env.ANNOUNCEMENT_TEXT || '';
+    let announcement = '';
+    
+    // Create Edge Config client to access stored values
+    const client = createClient(process.env.EDGE_CONFIG);
+    
+    if (client) {
+      // Get the announcement value from Edge Config
+      announcement = await client.get('announcement') || '';
+    }
     
     return NextResponse.json(
       { announcement },
