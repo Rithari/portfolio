af49243e738fd1b96b1b8e12bbfc82fc3bab64be
Leoluca D'Atri <22421051+Rithari@users.noreply.github.com>
2025-06-07 09:58:41 +0200

    Fix image reference for naveum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ const workExperiences = [
     company: "Naveum AG",
     role: "Development Operations Engineer",
     period: { start: "May 2025", end: "Present" },
-    logo: "/naveum.svg",
+    logo: "/naveum.png",
     locationText: "Zürich - Switzerland",
     tasks: [
       "Automating infrastructure deployment and management for cloud, on-prem and hybrid environments"
